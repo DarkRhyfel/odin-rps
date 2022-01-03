901212b4c09b4907b21f503aedcc8bebbd4dee50
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-01-03 10:54:50 -0500

    Added a function to play a quick game.
    Created a new function to plya a game of x rounds. Also updated rounds.

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -11,33 +11,69 @@ function playRound(playerPlay, computerPlay) {
     playerPlay = playerPlay.toLowerCase();
 
     if (playerPlay === computerPlay) {
-        return 'It\'s a tie!';
+        return {
+            result: 2,
+            message: 'It\'s a tie!'
+        };
     } else {
         switch (playerPlay) {
             case 'paper':
-                if (computerPlay === 'rock') {
-                    return 'Congratulations you win, paper beats rock!';
-                } else {
-                    return 'You lose, scissors beats paper!';
-                }
+                return {
+                    result: Number(computerPlay === 'rock'),
+                    message: displayResult(playerPlay, computerPlay, computerPlay === 'rock')
+                };
             case 'rock':
-                if (computerPlay === 'scissors') {
-                    return 'Congratulations you win, rock beats scissors!';
-                } else {
-                    return 'You lose, paper beats rock!';
-                }
+                return {
+                    result: Number(computerPlay === 'scissors'),
+                    message: displayResult(playerPlay, computerPlay, computerPlay === 'scissors')
+                };
             case 'scissors':
-                if (computerPlay === 'paper') {
-                    return 'Congratulations you win, scissors beats paper!';
-                } else {
-                    return 'You lose, rock beats scissors!';
-                }
+                return {
+                    result: Number(computerPlay === 'paper'),
+                    message: displayResult(playerPlay, computerPlay, computerPlay === 'paper')
+                };
             default:
-                return 'Your input is not valid, use rock, paper or scissors';
+                return {
+                    result: -1,
+                    message: 'Your input is not valid, use rock, paper or scissors'
+                };
         }
     }
 }
 
-const playerPlay = 'paper';
-const computerPlays = computerPlay();
-console.log(`Test: ${playRound(playerPlay, computerPlays)}`);
+function displayResult(playerPlay, computerPlay, result) {
+    if (result) {
+        return `Congratulations you win, ${playerPlay} beats ${computerPlay}!`;
+    } else {
+        return `You lose pal, ${computerPlay} beats ${playerPlay}!`;
+    }
+}
+
+function game(rounds = 5) {
+    let playerScore = 0;
+    let computerScore = 0;
+    let ties = 0;
+
+    for (i = 1; i <= rounds; i++) {
+        let playerPlay = prompt('Enter your selection to play!', 'paper');
+
+        let round = playRound(playerPlay, computerPlay());
+
+        console.log(round.message);
+
+        if (round.result === 2) {
+            ties += 1;
+        } else if (round.result === 1) {
+            playerScore += 1;
+        } else if (round.result === 0) {
+            computerScore += 1;
+        }
+    }
+
+    console.log(`Final Scores \
+    Computer Score: ${computerScore} \
+    Player Score: ${playerScore} \
+    Ties: ${ties}`);
+}
+
+game();
