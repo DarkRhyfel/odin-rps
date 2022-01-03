0fd1c620f52a2dfc45c0bf1cc5824bdd9ed70b98
Andrés Guerra <andres.guerra.95@protonmail.com>
2022-01-03 10:32:18 -0500

    Added function to play a round.
    Created a function that takes the players choice to play a round of RPS.

--- scripts/rock-paper-scissors.js
+++ scripts/rock-paper-scissors.js
@@ -6,3 +6,38 @@ function computerPlay() {
 
     return selection;
 }
+
+function playRound(playerPlay, computerPlay) {
+    playerPlay = playerPlay.toLowerCase();
+
+    if (playerPlay === computerPlay) {
+        return 'It\'s a tie!';
+    } else {
+        switch (playerPlay) {
+            case 'paper':
+                if (computerPlay === 'rock') {
+                    return 'Congratulations you win, paper beats rock!';
+                } else {
+                    return 'You lose, scissors beats paper!';
+                }
+            case 'rock':
+                if (computerPlay === 'scissors') {
+                    return 'Congratulations you win, rock beats scissors!';
+                } else {
+                    return 'You lose, paper beats rock!';
+                }
+            case 'scissors':
+                if (computerPlay === 'paper') {
+                    return 'Congratulations you win, scissors beats paper!';
+                } else {
+                    return 'You lose, rock beats scissors!';
+                }
+            default:
+                return 'Your input is not valid, use rock, paper or scissors';
+        }
+    }
+}
+
+const playerPlay = 'paper';
+const computerPlays = computerPlay();
+console.log(`Test: ${playRound(playerPlay, computerPlays)}`);
